8c013feb77e47f81ba102ba5b66960357ade8b5f
josefinaalvarez <68783618+josefinaalvarez@users.noreply.github.com>
2023-12-03 19:10:17 -0300

    Update AuthProvider.jsx

--- cause-connect/src/auth/AuthProvider.jsx
+++ cause-connect/src/auth/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useContext } from "react";
 import { AuthContext } from "./AuthContext";
-import { jwt_decode } from "jwt-decode";
+import * as jwtDecode from "jwt-decode";
 
 
 
